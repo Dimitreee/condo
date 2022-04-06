2e2aa64ce5cec66015782b3d7a7b264688fe993e
Pavel <nomerdvadcatpyat@gmail.com>
2022-04-06 17:24:30 +0500

    DOMA-2636 refactored update comment logic

--- apps/condo/pages/ticket/[id]/index.tsx
+++ apps/condo/pages/ticket/[id]/index.tsx
@@ -478,7 +478,9 @@ export const TicketPageContent = ({ organization, employee, TicketContent }) => 
         // @ts-ignore
         sortBy: ['createdAt_DESC'],
     })
-    const updateComment = TicketComment.useUpdate({})
+    const updateComment = TicketComment.useUpdate({}, () => {
+        refetchComments()
+    })
     const deleteComment = TicketComment.useSoftDelete({}, () => {
         refetchComments()
     })
@@ -692,6 +694,7 @@ export const TicketPageContent = ({ organization, employee, TicketContent }) => 
                                 <Comments
                                     // @ts-ignore
                                     createAction={createCommentAction}
+                                    updateAction={updateComment}
                                     comments={comments}
                                     canCreateComments={get(auth, ['user', 'isAdmin']) || get(employee, ['role', 'canManageTicketComments'])}
                                     actionsFor={comment => {
